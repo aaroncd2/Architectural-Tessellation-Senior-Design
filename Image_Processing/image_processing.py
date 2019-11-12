755758a788a2a24471c73df19fd247feb99ce48f
Kevin Mattappally <kevinmat@outlook.com>
2019-11-12 15:21:04 -0800

    removed extra code corresponding to matplotlib

--- Image_Processing/image_processing.py
+++ Image_Processing/image_processing.py
@@ -2,16 +2,11 @@ import numpy as np
 from cv2 import cv2 as cv
 from matplotlib import pyplot as plt
 
-def printHello():
-    print("hello")
-
 def processImage(image):
     sketch = cv.imread(image)
     imgray = cv.cvtColor(sketch, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(imgray, 127, 255, 0)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    # cv.drawContours(sketch, contours, -1, (0,255,0), 3)
-    # plt.subplot(121),plt.imshow(sketch, cmap = 'gray')
     coords = list()
     numVertices = len(contours[0])
     for i in range (0, len(contours[0])):
@@ -19,7 +14,6 @@ def processImage(image):
         coords.append(contours[0][i][0][0])
         # y coordinate
         coords.append(contours[0][i][0][1])
-    # cv.imshow('Output Sketch', sketch)
     cv.waitKey(0)
     cv.destroyAllWindows()
     return coords
