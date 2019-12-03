da4edf8615849bf9cee355737f4bf84ec3d9c21b
Kevin Mattappally <kevinmat@outlook.com>
2019-12-02 16:38:15 -0800

    Output of contours and houghline

--- Image_Processing/image_processing.py
+++ Image_Processing/image_processing.py
@@ -2,12 +2,11 @@ import numpy as np
 from cv2 import cv2 as cv
 from matplotlib import pyplot as plt
 
-def getHough(image):
-    lines = cv.HoughLinesP(image, 1, np.pi/180, 100, minLineLength=100, maxLineGap=10)
+def getHough(originalImg, cannyImg):
+    lines = cv.HoughLinesP(cannyImg, 1, np.pi/180, 100, minLineLength=100, maxLineGap=10)
     for line in lines:
         x1,y1,x2,y2 = line[0]
-        cv.line(image, (x1,y1),(x2,y2),(0,255,0),2)
-    return image
+        cv.line(originalImg, (x1,y1),(x2,y2),(0,0,255),2)
 
 def getCanny(image, sigma_val=0.33):
     #get median of single channel pixel image
@@ -24,13 +23,12 @@ def processImage(image):
     imgray = cv.cvtColor(sketch, cv.COLOR_BGR2GRAY)
     imgray = cv.bilateralFilter(imgray, 11, 17, 17)
     cannyImg = getCanny(imgray)
-    lines = cv.HoughLinesP(cannyImg, 1, np.pi/180, 100, minLineLength=100, maxLineGap=10)
-    for line in lines:
-        x1,y1,x2,y2 = line[0]
-        cv.line(sketch, (x1,y1),(x2,y2),(0,0,255),4)
-    cv.imshow("Canny + HoughLine Output", sketch)
+    getHough(sketch, cannyImg)
+    cv.imshow("hougline", sketch)
     ret, thresh = cv.threshold(cannyImg, 127, 255, 0)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cv.drawContours(sketch, contours, -1, (255,0,0), 3)
+    cv.imshow("contours", sketch)
     coords = list()
     numVertices = len(contours[0])
     for i in range (0, numVertices):
