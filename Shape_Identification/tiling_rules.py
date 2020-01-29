e41e929898ceadd97c5a3cacc01d56364b8ef443
Kevin Mattappally <kevinmat@outlook.com>
2020-01-28 17:18:14 -0800

    tiling rules file created

---
+++ Shape_Identification/tiling_rules.py
@@ -0,0 +1,13 @@
+from shapely.geometry import Polygon
+from shapely import affinity
+from shapely.geometry import Point
+import matplotlib.pyplot as plt
+import math
+from shapely.geometry import MultiPoint
+import numpy as np
+
+def numSides(shape):
+    pass
+
+def isRegular(shape):
+    pass
