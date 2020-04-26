7e1b84352f656151bf92f6da508ce2186de36c6c
Kevin Mattappally <kevinmat@outlook.com>
2020-04-26 14:52:33 -0700

    Rotation loop changed for convex quad case

--- Shape_Identification/tiling_rules.py
+++ Shape_Identification/tiling_rules.py
@@ -67,6 +67,8 @@ def process_quadrilateral(shape):
         max_length = max(side1_length, side2_length, side3_length, side4_length)
         # rotate coordinates to get the longest side between the 
         # zeroth and first coordinates
+        if max_length == side1_length:
+            rotations = 0
         if max_length == side2_length:
             rotations = 1
         elif max_length == side3_length:
@@ -109,10 +111,13 @@ def process_quadrilateral(shape):
         slope_3 = (second_rec_coords[3][1] - second_rec_coords[2][1]) / (second_rec_coords[3][0] - second_rec_coords[2][0])
         slope_4 = (second_rec_coords[4][1] - second_rec_coords[3][1]) / (second_rec_coords[4][0] - second_rec_coords[3][0])
         slope_list = [slope_1, slope_2, slope_3, slope_4]
+        # print statement for debugging
+        for i in range(0, len(second_rec_coords)):
+            print('coord [', i, '] = ', second_rec_coords[i])
         slope_list.sort()
         if second_max_length == side2_length:
+            print('second max length is side 2')
             # checking slopes for case with max side1 and max side2
-            if slope
             second_rec_coords.append((second_rec_coords[0][0] - second_to_first_vertex_len_x, second_rec_coords[0][1] - second_to_first_vertex_len_y))
             second_rec_coords.append(second_rec_coords[2])
             second_rec_coords.append(second_rec_coords[3])
@@ -124,6 +129,7 @@ def process_quadrilateral(shape):
             second_rec_exterior_coords.append(second_rec_coords[0])
             recommendations.append((Polygon(second_rec_coords), "parallelogram", True, Polygon(second_rec_exterior_coords)))
         elif second_max_length == side4_length:
+            print('second max length is side 4')
             zeroth_to_first_vertex_len_x = second_rec_coords[1][0] - second_rec_coords[0][0]
             zeroth_to_first_vertex_len_y = second_rec_coords[1][1] - second_rec_coords[0][1]
             second_rec_coords.append(second_rec_coords[3])
